d0b4f22d4390a23d62ce1e883ac6eab21c4c8d4b
Lior Abir <liorabir909@gmail.com>
2022-06-30 14:43:26 +0300

    icons

--- js/apps/note-app/cmps/note-add.cmp.js
+++ js/apps/note-app/cmps/note-add.cmp.js
@@ -3,21 +3,22 @@ import { notesService } from "../services/note-service.cmp.js"
 export default {
     template: `
             <form @submit.prevent="save" class="add-note-container">
-               <div class="add-new-note-input">
-                 <input type="text" :placeholder="setInputPlaceHolder" v-model="newNote.info.txt"/>
-                   <div class="add-actions-btns">
-                       <button @click="setNewType('note-txt')" title="text note">txt</button>
-                       <button @click="setNewType('note-img')" title="image note">img</button>
-                       <button @click="setNewType('note-todos')" title="to do note">todos</button>
-                   </div>
-              </div>
+                <div class="add-new-note-input">
+                   <input type="text" :placeholder="setInputPlaceHolder" v-model="txt"/>
+                      <div class="add-actions-btns">
+                        <button @click="setNewType('note-txt')" title="text note" type="button"><i class="fa-solid fa-comment"></i></button>
+                        <button @click="setNewType('note-img')" title="image note" type="button"><i class="fa-solid fa-image"></i></button>
+                        <button @click="setNewType('note-todos')" title="to do note" type="button"><i class="fa-solid fa-list"></i></button>
+                     </div>
+               </div>
                <button @click="save" type="submit">save</button>
             </form>
           `,
     props: [],
     data() {
         return {
-            newNote: notesService.getEmptyNote()
+            newNote: notesService.getEmptyNote(),
+            txt: ''
         }
     },
     methods: {
@@ -25,7 +26,9 @@ export default {
             this.newNote.type = newType
         },
         save() {
-            notesService.save(this.newNote).then(note =>{
+            this.newNote.info.txt = this.txt
+            notesService.save(this.newNote).then(note => {
+                console.log(note);
                 this.$emit('saved', note)
             })
             this.newNote = notesService.getEmptyNote()
@@ -35,17 +38,26 @@ export default {
         setInputPlaceHolder() {
             if (this.newNote.type === 'note-txt') {
                 this.newNote.info = {
-                    txt:''
+                    txt: this.txt
                 }
                 return 'Enter text...'
             }
             if (this.newNote.type === 'note-img') {
                 this.newNote.info = {
-                    txt:''
+                    URL: this.txt
                 }
                 return 'Enter image URL...'
             }
             if (this.newNote.type === 'note-todos') {
+                this.newNote.info = {
+                    URL: this.txt
+                }
+
+                // label: "Get my stuff together",
+                // todos: [
+                //     { txt: "Driving liscence", doneAt: null },
+                //     { txt: "Coding power", doneAt: 187111111 }
+                // ]
                 return 'Enter comma separated list...'
             }
         },
